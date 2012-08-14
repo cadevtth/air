17b4d50edf94d433c86030d660de45cbacae6a23
Raghu Betina <rbetina@gmail.com>
2012-08-14 11:50:55 -0500

    Introducing fuzzy search.

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,12 +3,18 @@ class FlightsController < ApplicationController
   def index
     
     if params[:keyword].present?
-      @flights = Flight.find_all_by_departure_code(params[:keyword].upcase)
+      # @flights = Flight.find_all_by_departure_code(params[:keyword].upcase)
+      
+      # @flights = Flight.where(:departure_code => params[:keyword])
+      # @flights = Flight.where("departure_code = ?", pams[:keyword])
+      # @flights = Flight.where("departure_code = ? OR arrival_code = ?", params[:keyword], params[:keyword])
+      # @flights = Flight.where("departure_code LIKE ?", "#{params[:keyword]}")
+      
+      @flights = Flight.where("departure_code LIKE ?", "%#{params[:keyword]}%").page(params[:page]).per(2)
+      
     else
-      @flights = Flight.all
+      @flights = Flight.page(params[:page]).per(2)
     end
-    
-    # @flights = Flight.page(params[:page]).per(2)
     
     # ?page=5
     # @page = params[:page].to_i
