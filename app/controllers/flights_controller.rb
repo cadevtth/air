5925143432c35ca26a500a139eeb38a79f39b971
Jeff Cohen <jeff@codeacademy.org>
2012-08-15 15:22:45 -0500

    Fixed bug in flights controller.

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,9 +3,9 @@ class FlightsController < ApplicationController
   def index
     if params[:keyword].present?
       keyword = params[:keyword].upcase
-      airport = Airport.where("UPPER(city) LIKE ? OR UPPER(code) LIKE ?", "%#{keyword}%", "%#{keyword}%")
+      airport = Airport.where("UPPER(city) LIKE ? OR UPPER(code) LIKE ?", "%#{keyword}%", "%#{keyword}%").first
       
-      @flights = airport.departing_flights.page(params[:page]).per(25).first
+      @flights = airport.departing_flights.page(params[:page]).per(25)
     else
       @flights = Flight.page(params[:page]).per(25)
     end
